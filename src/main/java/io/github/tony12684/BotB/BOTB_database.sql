53a0d3246a159cc95b37f71a7b06d0f8578c583f
Tony Lashbrook <tonylashbrook@Yahoo.com>
2025-10-17 21:06:07 -0500

    Update BOTB_database to rebuild a partially dropped database

--- src/main/java/io/github/tony12684/BotB/BOTB_database.sql
+++ src/main/java/io/github/tony12684/BotB/BOTB_database.sql
@@ -1,24 +1,24 @@
 --** = (Foreign key item)
-CREATE TABLE `users` ( --minecraft UUID without dashes
+CREATE TABLE IF NOT EXISTS `users` ( --minecraft UUID without dashes
   `uuid` char(32) PRIMARY KEY,
-  `username` varchar(16) NOT NULL,
+  `username` varchar(16) UNIQUE NOT NULL,
   `nickname` varchar(255)
 );
 
-CREATE TABLE `games` ( --each played game
+CREATE TABLE IF NOT EXISTS `games` ( --each played game
   `game_id` int PRIMARY KEY AUTO_INCREMENT,
   `game_start_date_time` datetime NOT NULL, --yyyy-MM-ddTHH:mm:ss
   `game_end_date_time` datetime, --yyyy-MM-ddTHH:mm:ss
   `team_id` int --**, the winning team
 );
 
-CREATE TABLE `user_games` ( --which users played in which games
+CREATE TABLE IF NOT EXISTS `user_games` ( --which users played in which games
   `user_games_id` int PRIMARY KEY AUTO_INCREMENT,
   `game_id` int NOT NULL, --**
   `uuid` char(32) NOT NULL --**
 );
 
-CREATE TABLE `seats` ( --which seat each user had in each game
+CREATE TABLE IF NOT EXISTS `seats` ( --which seat each user had in each game
   `seat_id` int PRIMARY KEY AUTO_INCREMENT,
   `position` int NOT NULL,
   `uuid` char(32) NOT NULL, --**
@@ -26,7 +26,7 @@ CREATE TABLE `seats` ( --which seat each user had in each game
   `action_id` int --**, optional, the action that caused the seat change
 ); --in the case of seat changes, multiple entries per user per game
 
-CREATE TABLE `user_game_roles` ( --which roles each user had in each game
+CREATE TABLE IF NOT EXISTS `user_game_roles` ( --which roles each user had in each game
   `user_game_roles_id` int PRIMARY KEY AUTO_INCREMENT,
   `role_id` int NOT NULL, --**
   `game_id` int NOT NULL, --**
@@ -34,7 +34,7 @@ CREATE TABLE `user_game_roles` ( --which roles each user had in each game
   `action_id` int --**, optional, the action that caused the role change
 ); --in the case of role changes, multiple entries per user per game
 
-CREATE TABLE `user_game_teams` ( --which teams each user was on in each game
+CREATE TABLE IF NOT EXISTS `user_game_teams` ( --which teams each user was on in each game
   `user_game_teams_id` int PRIMARY KEY AUTO_INCREMENT,
   `game_id` int NOT NULL, --**
   `uuid` char(32) NOT NULL, --**
@@ -43,7 +43,7 @@ CREATE TABLE `user_game_teams` ( --which teams each user was on in each game
 ); --in the case of team changes, multiple entries per user per game
 
 
-CREATE TABLE `actions` (--all documented actions taken by users in games
+CREATE TABLE IF NOT EXISTS `actions` (--all documented actions taken by users in games
   `action_id` int PRIMARY KEY AUTO_INCREMENT,
   `game_id` int NOT NULL, --**
   `uuid` char(32) NOT NULL, --**
@@ -57,7 +57,7 @@ CREATE TABLE `actions` (--all documented actions taken by users in games
   `action_notes` varchar(255)
 );
 
-CREATE TABLE `targets` ( --which users were targeted by a specific action
+CREATE TABLE IF NOT EXISTS `targets` ( --which users were targeted by a specific action
   `target_id` int PRIMARY KEY AUTO_INCREMENT,
   `action_id` int NOT NULL, --**
   `uuid` char(32) NOT NULL --**
@@ -65,47 +65,47 @@ CREATE TABLE `targets` ( --which users were targeted by a specific action
 
 
 
-CREATE TABLE `roles` ( --reference table for all roles
+CREATE TABLE IF NOT EXISTS `roles` ( --reference table for all roles
   `role_id` int PRIMARY KEY AUTO_INCREMENT,
   `role_name` varchar(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE `teams` ( --reference table for all teams
+CREATE TABLE IF NOT EXISTS `teams` ( --reference table for all teams
   `team_id` int PRIMARY KEY AUTO_INCREMENT,
   `team_name` varchar(255) UNIQUE NOT NULL
 );
 
 --TODO add indexes for performance if needed
 ALTER TABLE `games`
-  ADD FOREIGN KEY (`team_id`) REFERENCES `teams`(`team_id`);
+  ADD FOREIGN KEY IF NOT EXISTS (`team_id`) REFERENCES `teams`(`team_id`);
 
 ALTER TABLE `user_game_roles`
-  ADD FOREIGN KEY (`role_id`) REFERENCES `roles`(`role_id`),
-  ADD FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`),
-  ADD FOREIGN KEY (`uuid`) REFERENCES `users`(`uuid`),
-  ADD FOREIGN KEY (`action_id`) REFERENCES `actions`(`action_id`);
+  ADD FOREIGN KEY IF NOT EXISTS (`role_id`) REFERENCES `roles`(`role_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`game_id`) REFERENCES `games`(`game_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`uuid`) REFERENCES `users`(`uuid`),
+  ADD FOREIGN KEY IF NOT EXISTS (`action_id`) REFERENCES `actions`(`action_id`);
 
 ALTER TABLE `user_game_teams`
-  ADD FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`),
-  ADD FOREIGN KEY (`uuid`) REFERENCES `users`(`uuid`),
-  ADD FOREIGN KEY (`team_id`) REFERENCES `teams`(`team_id`),
-  ADD FOREIGN KEY (`action_id`) REFERENCES `actions`(`action_id`);
+  ADD FOREIGN KEY IF NOT EXISTS (`game_id`) REFERENCES `games`(`game_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`uuid`) REFERENCES `users`(`uuid`),
+  ADD FOREIGN KEY IF NOT EXISTS (`team_id`) REFERENCES `teams`(`team_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`action_id`) REFERENCES `actions`(`action_id`);
 
 ALTER TABLE `seats`
-  ADD FOREIGN KEY (`uuid`) REFERENCES `users`(`uuid`),
-  ADD FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`),
-  ADD FOREIGN KEY (`action_id`) REFERENCES `actions`(`action_id`);
+  ADD FOREIGN KEY IF NOT EXISTS (`uuid`) REFERENCES `users`(`uuid`),
+  ADD FOREIGN KEY IF NOT EXISTS (`game_id`) REFERENCES `games`(`game_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`action_id`) REFERENCES `actions`(`action_id`);
 
 ALTER TABLE `actions`
-  ADD FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`),
-  ADD FOREIGN KEY (`uuid`) REFERENCES `users`(`uuid`),
-  ADD FOREIGN KEY (`team_id`) REFERENCES `teams`(`team_id`),
-  ADD FOREIGN KEY (`role_id`) REFERENCES `roles`(`role_id`);
+  ADD FOREIGN KEY IF NOT EXISTS (`game_id`) REFERENCES `games`(`game_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`uuid`) REFERENCES `users`(`uuid`),
+  ADD FOREIGN KEY IF NOT EXISTS (`team_id`) REFERENCES `teams`(`team_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`role_id`) REFERENCES `roles`(`role_id`);
 
 ALTER TABLE `user_games`
-  ADD FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`),
-  ADD FOREIGN KEY (`uuid`) REFERENCES `users`(`uuid`);
+  ADD FOREIGN KEY IF NOT EXISTS (`game_id`) REFERENCES `games`(`game_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`uuid`) REFERENCES `users`(`uuid`);
 
 ALTER TABLE `targets`
-  ADD FOREIGN KEY (`action_id`) REFERENCES `actions`(`action_id`),
-  ADD FOREIGN KEY (`uuid`) REFERENCES `users`(`uuid`);
+  ADD FOREIGN KEY IF NOT EXISTS (`action_id`) REFERENCES `actions`(`action_id`),
+  ADD FOREIGN KEY IF NOT EXISTS (`uuid`) REFERENCES `users`(`uuid`);
