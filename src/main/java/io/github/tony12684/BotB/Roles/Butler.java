30ab065c5192d15e8075e01da13abac34f58c622
tony12684 <tonylashbrook@yahoo.com>
2025-11-04 12:08:36 -0600

    Began empath and butler implementation

--- src/main/java/io/github/tony12684/BotB/Roles/Butler.java
+++ src/main/java/io/github/tony12684/BotB/Roles/Butler.java
@@ -1,6 +1,7 @@
 package io.github.tony12684.BotB.Roles;
 import io.github.tony12684.BotB.Role;
 import io.github.tony12684.BotB.Game;
+import io.github.tony12684.BotB.PlayerPerformer;
 
 /*
  * Butler - Outsider
@@ -11,23 +12,26 @@ import io.github.tony12684.BotB.Game;
  */
 
 public class Butler extends Role {
+
     public Butler() {
         super("Butler", "Outsider");
     }
 
     @Override
     public boolean firstNightAction(Game game) {
-        return pickMaster();
+        return pickMaster(game);
     }
 
     @Override
     public boolean otherNightAction(Game game) {
-        return pickMaster();
+        return pickMaster(game);
 
     }
 
-    private boolean pickMaster() {
-        // TODO implement Butler pick master logic
+    private boolean pickMaster(Game game) {
+        // Prompt the Butler to pick a master player
+        // TODO get user input that returns an in game player alive or dead except themselves
+        // this.master = chosenPlayer;
         return true;
     }
 }
--- src/main/java/io/github/tony12684/BotB/Roles/Empath.java
+++ src/main/java/io/github/tony12684/BotB/Roles/Empath.java
@@ -13,4 +13,60 @@ public class Empath extends Role {
     public Empath() {
         super("Empath", "Townsfolk");
     }
+    @Override
+    public boolean firstNightAction(Game game) {
+        boolean result = empathy(Game game);
+        return true;
+    }
+    @Override
+    public boolean otherNightAction(Game game) {
+        boolean result = empathy(Game game);
+        return true;
+    }
+
+    private boolean empathy(Game game) {
+        // Returns the two neighbors of the given player based on seat order
+        getLivingNeighbors(Game game, game.getPlayerByRole("Empath"));
+
+        return true;
+    }
+
+    private getLivingNeighbors(Game game, PlayerPerformer empath) {
+        List<PlayerPerformer> players = game.getPlayers();
+        players = game.sortPlayersBySeatOrder(players);
+        Int index = empath.getSeat(); // Seats are 1-indexed
+        PlayerPerformer leftNeighbor;
+        PlayerPerformer rightNeighbor;
+
+        if (index == 1) {
+            for (int i = 1; i =< players.size(); i++) {
+                //check each players to the right of the empath from max to min
+                PlayerPerformer player = players.get(players.size() - i);
+                if (player.isAlive()) {
+                    rightNeighbor = player;
+                    break;
+                }
+            }
+        } else {
+            leftNeighbor = players.get(index - 2); // -2 for 0-indexed list
+        }
+
+        if (index == players.size()) {
+            rightNeighbor = players.get(0); // Wrap around to first player
+        } else {
+            rightNeighbor = players.get(index); // index is already 0-indexed for right neighbor
+        }
+
+        List<PlayerPerformer> livingNeighbors = new ArrayList<>();
+        if (leftNeighbor.isAlive()) {
+            livingNeighbors.add(leftNeighbor);
+        }
+        if (rightNeighbor.isAlive()) {
+            livingNeighbors.add(rightNeighbor);
+        }
+        return livingNeighbors;
+
+    }
+
+
 }
