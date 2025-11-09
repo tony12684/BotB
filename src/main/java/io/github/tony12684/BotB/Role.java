210da89a010d46f6b4ecb65ba045d9612d73bfea
Tony Lashbrook <tonylashbrook@Yahoo.com>
2025-11-09 09:23:32 -0600

    Added Affiliation and Team enums and expanded vote actions

--- src/main/java/io/github/tony12684/BotB/Role.java
+++ src/main/java/io/github/tony12684/BotB/Role.java
@@ -2,19 +2,37 @@ package io.github.tony12684.BotB;
 
 public class Role {
     private String roleName; // Name of the role
-    private String team; // Default team affiliation
+    private Affiliation affiliation; // Townsfolk, Outsider, Minion, Demon, Traveler, etc.
+    private Team team; // Good or Evil team
     private String startingMessage = "Have fun!"; // Message sent to player when assigned this role
     private int actionPriority; // Priority of the role's action during the night phase (lower number = higher priority)
     private Role falseRole; // A false role to display to the player instead of their actual role
 
-    public Role(String roleName, String team) {
+    public enum Affiliation {
+        TOWNSFOLK,
+        OUTSIDER,
+        MINION,
+        DEMON,
+        TRAVELER,
+        STORYTELLER
+    }
+
+    public enum Team {
+        GOOD,
+        EVIL,
+        STORYTELLER
+    }
+
+    public Role(String roleName, Affiliation affiliation, Team team) {
         // Constructor for Role class
         this.roleName = roleName;
+        this.affiliation = affiliation;
         this.team = team;
     }
-    public Role(String roleName, String team, String startingMessage) {
+    public Role(String roleName, Affiliation affiliation, Team team, String startingMessage) {
         // Constructor for Role class with starting message
         this.roleName = roleName;
+        this.affiliation = affiliation;
         this.team = team;
         this.startingMessage = startingMessage;
     }
@@ -31,12 +49,19 @@ public class Role {
         this.roleName = roleName;
     }
 
-    public String getTeam() {
+    public Team getTeam() {
         return team;
     }
 
-    public void setTeam(String team) {
+    public void setTeam(Team team) {
         this.team = team;
+    }
+
+    public Affiliation getAffiliation() {
+        return affiliation;
+    }
+    public void setAffiliation(Affiliation affiliation) {
+        this.affiliation = affiliation;
     }
 
     public Role getFalseRole() {
@@ -45,35 +70,7 @@ public class Role {
     public void setFalseRole(Role falseRole) {
         this.falseRole = falseRole;
     }
-
-    public boolean setup(Game game) {
-        // Default implementation does nothing
-        // Override this method in subclasses for specific setup logic
-        // return true if setup was performed successfully, false otherwise
-        return false;
-    }
-
-    public boolean firstNightAction(Game game) {
-        // Default implementation does nothing
-        // Override this method in subclasses for specific first night actions
-        // return true if action was performed successfully, false otherwise
-        return false;
-    }
-
-    public boolean otherNightAction(Game game){
-        // Default implementation does nothing
-        // Override this method in subclasses for specific other night actions
-        // return true if action was performed successfully, false otherwise
-        return false;
-    }
-
-    public boolean voteAction(Game game) {
-        // Default implementation does nothing
-        // Override this method in subclasses for specific vote actions
-        // return true if action was performed successfully, false otherwise
-        return false;
-    }
-
+    
     public int getActionPriority() {
         return actionPriority;
     }
@@ -81,4 +78,33 @@ public class Role {
     public void setActionPriority(int actionPriority) {
         this.actionPriority = actionPriority;
     }
+
+    // TODO overhaul setup and action methods to return information nessessary for logging the actions
+    public boolean setup(Game game) {
+        return false;
+    }
+
+    public boolean firstNightAction(Game game) {
+        return false;
+    }
+
+    public boolean otherNightAction(Game game){
+        return false;
+    }
+
+    public boolean voteAction(Game game) {
+        // For when you place a vote on a nominee
+        return false;
+    }
+
+    public boolean voteNominateAction(Game game) {
+        // For when you nominate a player for execution
+        return false;
+    }
+
+    public boolean voteNomineeAction(Game game) {
+        // For when you are nominated for execution
+        return false;
+    }
+
 }
