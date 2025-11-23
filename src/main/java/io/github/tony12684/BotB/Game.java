86d8b7127d03b6e6d8b61aceae2d82d776322e5c
Tony Lashbrook <tonylashbrook@Yahoo.com>
2025-11-22 18:56:02 -0600

    Built first night and setup functionality

--- src/main/java/io/github/tony12684/BotB/Game.java
+++ src/main/java/io/github/tony12684/BotB/Game.java
@@ -1,13 +1,16 @@
 package io.github.tony12684.BotB;
 import io.github.tony12684.BotB.Role.Affiliation;
+import io.github.tony12684.BotB.Role.Team;
 import io.github.tony12684.BotB.Roles.Storyteller;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import net.md_5.bungee.api.ChatColor;
 
@@ -21,12 +24,16 @@ public class Game {
     private Main plugin;
     private int gameId;
     
-    public Game(Main plugin, String storytellerUUID, List<String> playerUUIDs) {
+    public Game(Main plugin, UUID storytellerUUID, List<UUID> playerUUIDs) {
         //Constructor for Game class
         //Responsible for game setup sequencing
-
         this.gameState = "setup";
         this.dayCount = 0;
+        if (plugin.debugMode) {
+            Bukkit.getLogger().info("Initializing new game setup.");
+            Bukkit.getLogger().info("gamestate set to 'setup'.");
+            Bukkit.getLogger().info("dayCount set to 0.");
+        }
         this.plugin = plugin;
         try {
             this.gameId = plugin.insertGameStart();
@@ -34,32 +41,35 @@ public class Game {
         } catch (Exception e) {
             crashGame("Database error on game start: " + e.getMessage(), storytellerUUID);
         }
-
         // TODO update this to accept fabled storytellers
-        this.storyteller = new StorytellerPerformer(storytellerUUID, new Storyteller(), Bukkit.getPlayer(storytellerUUID).getName());
+        // TODO update this to accept storyteller nicknames
+        this.storyteller = new StorytellerPerformer(storytellerUUID, null, Bukkit.getPlayer(storytellerUUID).getName());
+        storyteller.setRole(new Storyteller(storyteller));
         this.grimoire = new Grimoire(storyteller, this);
 
 
         // Build players list
-        for (String uuid : playerUUIDs) {
+        this.players = new ArrayList<>();
+        for (UUID uuid : playerUUIDs) {
             players.add(new PlayerPerformer(uuid, null, Bukkit.getPlayer(uuid).getName())); // Role to be assigned later
+            if (plugin.debugMode) {
+                Bukkit.getLogger().info("Added player to game: " + Bukkit.getPlayer(uuid).getName());
+            }
             // TODO adjust for nickname plugin support
         }
         // TODO log players to game_users
 
         //TODO probably move all this to Game.startGame()
         assignSeats(players);
-        linkNeighbors(players);
+        
+        // linkNeighbors(players);
 
-        List<Role> roleList = grimoire.buildRoleList(players.size());
+        List<Role> roleList = grimoire.buildRoleList(this, players.size());
         assignRoles(players, roleList);
 
-        // setupPhase();
-        // while loop through all setup actions and temp log their corresponding ActionLogs
-        // after the loop, process all setup ActionLogs
-        // build to a list of ActionLogs to process after all actions are taken
+        setupPhase();
 
-        firstNight(players); // Proceed to the first night phase
+        firstNight(); // Proceed to the first night phase
         //TODO build daytime, voting and subsequent night phases loop
         //TODO build drunk false role handling into Game class action logic
 
@@ -77,6 +87,10 @@ public class Game {
 
     public Grimoire getGrimoire() {
         return grimoire;
+    }
+
+    public Main getPlugin() {
+        return plugin;
     }
 
     public PlayerPerformer getPlayerByRole(String roleName) {
@@ -112,6 +126,9 @@ public class Game {
         Collections.shuffle(players);
         for (PlayerPerformer player : players) {
             player.setSeat(players.indexOf(player) + 1); // Seats numbered from 1
+            if (plugin.debugMode) {
+                Bukkit.getLogger().info("Assigned seat " + player.getSeat() + " to player " + player.getName());
+            }
         } // Unless players list is shuffled, order should match seat number.
         //TODO validate with storyteller
         //TODO manual seat assignment option
@@ -149,30 +166,38 @@ public class Game {
         for (PlayerPerformer player : players) {
             if (!roleList.isEmpty()) {
                 player.setRole(roleList.remove(0));
+                if (plugin.debugMode) {
+                    Bukkit.getLogger().info("Assigned role " + player.getRole().getRoleNameActual() + " to player " + player.getName());
+                }
             } else {
                 crashGame("Not enough roles for players!", storyteller.getUUID());
             }
         }
         if (!roleList.isEmpty()) {crashGame("Unassigned roles remain!", storyteller.getUUID());}
         // TODO validate with storyteller
-        for (PlayerPerformer player : players) {
-            player.getRole().setup(this);
-        }
-        updateGrimoire(this);
     }
 
-    private List<Role> getBluffRoles() {
-        // Build and return a list of Role objects representing available bluff roles
-        List<Role> bluffRoles = new ArrayList<>();
-        //TODO retrieve list of bluff roles from storyteller
-        return bluffRoles;
+    private void setupPhase() {
+        // perform all setup()'s in roles
+        List<ActionLog> setupLogs = this.grimoire.setupLoop(this);
+        // if storyteller wants to do first night setup mode, do that now
+        if (this.grimoire.getFirstNightSetupMode()) {
+            List<ActionLog> firstNightSetupLogs = this.grimoire.firstNightSetupLoop(this);
+        }
     }
 
     private List<PlayerPerformer> getAllMinions(List<PlayerPerformer> players) {
         List<PlayerPerformer> minions = new ArrayList<>();
         for (PlayerPerformer player : players) {
-            if (player.getRole().getAffiliationActual().equals(Affiliation.MINION)) {
-                minions.add(player);
+            if (player.getRole().getFalseRole() != null) {
+                // Contiginency to add "drunk minions" though they don't yet exist
+                if (player.getRole().getFalseRole().getAffiliationActual().equals(Affiliation.MINION)) {
+                    minions.add(player);
+                }
+            } else {
+                if (player.getRole().getAffiliationActual().equals(Affiliation.MINION)) {
+                    minions.add(player);
+                }
             }
         }
         return minions;
@@ -181,10 +206,18 @@ public class Game {
     private List<PlayerPerformer> getDemons(List<PlayerPerformer> players) {
         // Return a list of all Demon players in the game
         // Pit Hag can make more than one Demon
+        // ALSO includes drunk demons like lunatic
         List<PlayerPerformer> demons = new ArrayList<>();
         for (PlayerPerformer player : players) {
-            if (player.getRole().getAffiliationActual().equals(Affiliation.DEMON)) {
-                demons.add(player);
+            if (player.getRole().getFalseRole() != null) {
+                // Contiginency to add "drunk demons" though they don't yet exist
+                if (player.getRole().getFalseRole().getAffiliationActual().equals(Affiliation.DEMON)) {
+                    demons.add(player);
+                }
+            } else {
+                if (player.getRole().getAffiliationActual().equals(Affiliation.DEMON)) {
+                    demons.add(player);
+                }
             }
         }
         return demons;
@@ -199,23 +232,68 @@ public class Game {
         return false;
     }
 
-    private void firstNight(List<PlayerPerformer> players) {
+    private void firstNight() {
         // Handle the first night phase of the game
-        this.gameState = "nighttime";
+        this.gameState = "night";
         this.dayCount = 1;
-        notifyPlayersOfRoles(players);
+        if (plugin.debugMode) {
+            Bukkit.getLogger().info("First night phase started.");
+            Bukkit.getLogger().info("gamestate set to 'night'.");
+            Bukkit.getLogger().info("dayCount set to 1.");
+        }
+        notifyPlayersOfRoles();
         List<PlayerPerformer> minions = getAllMinions(players);
         List<PlayerPerformer> demons = getDemons(players);
-        minionInfo(minions, demons.getFirst()); // Assumes 1 demon at start of game
-        List<Role> bluffs = getBluffRoles();
-        demonInfo(demons.getFirst(), minions, bluffs);
+        // notify minions of each other and true demon
+        for (PlayerPerformer demon : demons) {
+            // ignore lunatic among demons for minion info
+            if (demon.getRole().getAffiliationActual().equals(Affiliation.DEMON)) {
+                // only one demon should be true demon
+                // minions do not know about lunatic demons
+                grimoire.minionInfo(minions, demon);
+            }
+        }
+        List<Role> bluffs = grimoire.getBluffRoles();
+        for (PlayerPerformer demon : demons) {
+            // check for false demon
+            if (demon.getRole().getFalseRole() != null) {
+                PlayerPerformer trueDemon = null;
+                // find true demon
+                for (PlayerPerformer demonY : demons) {
+                    if (demonY.getRole().getAffiliationActual().equals(Affiliation.DEMON)) {
+                        trueDemon = demonY;
+                    }
+                }
+                // notify lunatic of minions and bluff roles
+                grimoire.demonInfo(null, demon, minions, bluffs);
+                // notify true demon of lunatic and minions and bluff roles
+                grimoire.demonInfo(trueDemon, demon, minions, bluffs);
+            } else {
+                // notify true demon of minions and bluff roles
+                grimoire.demonInfo(demon, null, minions, bluffs);
+            }
+        }
         // TODO add storyteller to action list to accomidate fabled roles
         sortPlayersByActionPriority(players);
         for (PlayerPerformer player : players) {
-            if (player.getDrunk() || player.getPoisoned()) {
-                //TODO implement drunk and poisoned logic
-                // Prompt storyteller for action resolution
+            if (player.getRole().getFalseRole() != null) {
+                // for false role players do their false roles false first night action
+                try {
+                    player.getRole().getFalseRole().falseFirstNightAction(this);
+                } catch (Exception e) {
+                    // Catch any exceptions thrown during the false first night action
+                    crashGame("Error during false first night action for player " + player.getUUID() + ": " + e.getMessage(), storyteller.getUUID());
+                }
+            } else if (player.getDrunk() || player.getPoisoned()) {
+                // for drunk or poisoned players do their false first night action
+                try {
+                    player.getRole().falseFirstNightAction(this);
+                } catch (Exception e) {
+                    // Catch any exceptions thrown during the false first night action
+                    crashGame("Error during false first night action for player " + player.getUUID() + ": " + e.getMessage(), storyteller.getUUID());
+                }
             } else {
+                // for normal players do their normal first night action
                 try {
                     player.getRole().firstNightAction(this);
                 } catch (Exception e) {
@@ -227,9 +305,9 @@ public class Game {
     }
 
     
-    private void notifyPlayersOfRoles(List<PlayerPerformer> players) {
+    private void notifyPlayersOfRoles() {
         // Notify each player of their assigned role
-        //TODO upgrade this to use our UI when we find one
+        // TODO send the messages to the grimoire to display in a better UI
         for (PlayerPerformer player : players) {
             Player bukkitPlayer = Bukkit.getPlayer(player.getUUID());
             if (bukkitPlayer != null && bukkitPlayer.isOnline()) {
@@ -250,69 +328,7 @@ public class Game {
         }
     }
 
-    private void minionInfo(List<PlayerPerformer> minions, PlayerPerformer demon) {
-        // Send minion and demon information to each minion player
-        //TODO upgrade this to use our UI when we find one
-        for (PlayerPerformer minion : minions) {
-            Player player = Bukkit.getPlayer(minion.getUUID());
-            if (player != null && player.isOnline()) {
-                StringBuilder info = new StringBuilder(ChatColor.DARK_RED + "Your fellow Minions are:\n");
-                for (PlayerPerformer fellowMinion : minions) {
-                    if (!fellowMinion.getUUID().equals(minion.getUUID())) {
-                        Player fellowPlayer = Bukkit.getPlayer(fellowMinion.getUUID());
-                        if (fellowPlayer != null) {
-                            info.append("- ").append(fellowPlayer.getName()).append("\n");
-                            //TODO modify this for nickname plugin support
-                        }
-                    }
-                }
-                player.sendMessage(info.toString());
-            } else {
-                crashGame("Minion player not found or offline in Game.minionInfo(): " + minion.getUUID(), storyteller.getUUID());
-            }
-        }
-    }
-
-    private void demonInfo(PlayerPerformer demon, List<PlayerPerformer> minions, List<Role> bluffRoles) {
-        // Send demon information to the demon player
-        //TODO upgrade this to use our UI when we find one
-        Player player = Bukkit.getPlayer(demon.getUUID());
-        if (player != null && player.isOnline()) {
-            StringBuilder info = new StringBuilder(ChatColor.DARK_RED + "You are the Demon.\n");
-            if (!minions.isEmpty()) {
-                info.append("Your Minions are:\n");
-                for (PlayerPerformer minion : minions) {
-                    Player minionPlayer = Bukkit.getPlayer(minion.getUUID());
-                    if (minionPlayer != null) {
-                        info.append("- ").append(minionPlayer.getName()).append("\n");
-                        //TODO modify this for nickname plugin support
-                    }
-                }
-            } else {
-                info.append("You have no Minions.\n");
-                // This should never happen in a standard game
-            }
-            if (!bluffRoles.isEmpty()) {
-                info.append("Available Bluff Roles:\n");
-                for (Role bluff : bluffRoles) {
-                    info.append("- ").append(bluff.getRoleNameActual()).append("\n");
-                }
-            } else {
-                // This should never happen in a standard game
-                info.append("No Bluff Roles available.\n");
-            }
-            player.sendMessage(info.toString());
-        } else {
-            crashGame("Demon player not found or offline in Game.demonInfo(): " + demon.getUUID(), storyteller.getUUID());
-        }
-    }
-
-    private void updateGrimoire(Game game) {
-        // Update the storyteller's grimoire with current game information
-        //TODO build this
-    }
-
-    public void crashGame(String reason, String storytellerUUID) {
+    public void crashGame(String reason, UUID storytellerUUID) {
         // Handle game crash scenario
         // TODO build a proper crash handler that effects game state without server reboot
         // TODO handle exceptions to try to keep the game running if possible
